9468dc6818095e69077d1fdfe207c3031a8feea0
cliu81530 <126965542+cliu81530@users.noreply.github.com>
2023-03-13 17:17:09 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,25 +3,7 @@ using System.Linq;
 
 namespace stringCalculator
 {
-    class StringCalculator
-    {
-        public int Calculate(string number)
-        {
 
-
-            return 0;
-            //if (splitNumbers.Length == 1)
-            //{
-            //    if(splitNumbers[0].Length==0)
-            //    {
-            //        return 0;
-            //    }
-            //    return int.Parse(splitNumbers[0]);
-            //}
-            //return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
-        }
-
-    }
 
     class Program
     {
