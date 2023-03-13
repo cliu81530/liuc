7405d9733bd1fd6212eacecec195809c52df7384
cliu81530 <126965542+cliu81530@users.noreply.github.com>
2023-03-13 17:05:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace stringCalculator
+{
+    class StringCalculator
+    {
+        public int Calculate(string number)
+        {
+
+
+            return 0;
+            //if (splitNumbers.Length == 1)
+            //{
+            //    if(splitNumbers[0].Length==0)
+            //    {
+            //        return 0;
+            //    }
+            //    return int.Parse(splitNumbers[0]);
+            //}
+            //return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
